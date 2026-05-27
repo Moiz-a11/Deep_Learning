e2943a32981d4619911e347a738dbe46eb827a19
moiz shaikh <moeez4286@gmail.com>
2026-05-28 00:44:36 +0530

    updated code

--- agent.py
+++ agent.py
@@ -2,21 +2,68 @@
 import flappy_bird_gymnasium
 import  gymnasium as gym
 from dqn import DQN
+from experieence_replay import ReplyMemory
+import itertools
+import yaml
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 
-def run(self,is_training=True,render=False):
-    env = gym.make("FlappyBird-v0",render_mode=  "human" if render else None)
-    num_states = env.observation_space.shape[0]
-    num_actions = env.action_space.n
-    policy_dqn = DQN(num_states,num_actions).to(device)
-    state, _  = env.reset()
-    while True:
-        action = env.action_space_sample()
+if torch.backends.mps.is_available():
+    device = "mps"
+elif torch.cuda.is_available():
+    device ="cuda"
+else:
+    device ="cpu"
 
-        obs, reward, terminated, _, _ = env.step(action)
+class Agent:
 
-        if terminated:
+    def __init__(self,param_set):
+        self.param_set = param_set
+        with open("parameters.yaml","r") as  f:
+            all_param_set = yaml.safe_load(f)
+            params =all_param_set[problem_set]
+        self.alpha = params["alpha"]
 
-            break
+        self.gamma = params["gamma"]
 
-    env.close()
+        self.epsilon_init = params["epsilon_init"]
+        self.epsilon_min = params["epsilon_min"]
+        self.epsilon_decay = params["epsilon_decay"]
+
+        self.replay_memory_size = params["replay_memory_size"]
+        self.mini_batch_size = params["mini_batch_size"]
+
+        self.reward_threshold = params["reward_threshold "]
+        self.network_sync_rate = params["network_sync_rate"]
+        self.mini_batch_size = params["mini_batch_size"]
+
+        self.loss_fn = nn.MSELoss()
+        self.optimizer = None
+
+    def run(self,is_training=True,render=False):
+
+        env = gym.make("FlappyBird-v0",render_mode=  "human" if render else None)
+        num_states = env.observation_space.shape[0]
+        num_actions = env.action_space.n
+        policy_dqn = DQN(num_states,num_actions).to(device)
+
+        if is_training:
+            memory = ReplyMemory(self.replay_memory_size)
+        for episode in itertools.count():
+            state, _  = env.reset()
+            episode_rewards = 0
+            terminated =False
+
+            while not terminated:
+                action = env.action_space_sample()
+
+                next_state , reward ,terminated, _, _ = env.step(action)
+                if is_training:
+                    memory.append((state,action,new_state,reward,terminated))
+                state= new_state
+                episode_rewards += rewards
+            print(f"for episode = {episode+1} total_reward = {episode_rewards}")
+
+   # env.close() manually stop  instead automatically 
